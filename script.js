db81ffe34a628db287b57c7d89db1a640e8ecf17
Minh Hiển Nguyễn <minhhien24022001@gmail.com>
2021-11-05 08:02:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ login.onclick = function() {
     login.classList.add('active');
 }
 
+let addsub = document.getElementById('addsub');
+addsub.onclick = function(event) {
+    let target = event.target;
+    if (target.closest('.icon-add')) {
+        let iconAdd = target.closest('.icon-add');
+        let quantity = iconAdd.nextElementSibling;
+        quantity.value = Number(quantity.value) + 1;
+    }
+    if (target.closest('.icon-sub')) {
+        let iconSub = target.closest('.icon-sub');
+        let quantity = iconSub.previousElementSibling;
+        if (Number(quantity.value) > 0) { 
+            quantity.value = Number(quantity.value) - 1;
+        }
+    }
+}
+
+
+//footer
+let heightOfWindow = document.documentElement.clientHeight;
+let heightOfBody = document.body.clientHeight;
+let footer = document.querySelector('footer');
+
+if (heightOfBody <= heightOfWindow) {
+    footer.classList.add('active');
+}
