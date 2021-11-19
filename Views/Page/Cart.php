29f9944aa0168e4b170802bc0e33837e4340b7f4
HoaiNam071001 <nam215543694@gmail.com>
2021-11-19 11:37:08 +0000

    Update Cart.php

--- Views/Page/Cart.php
+++ Views/Page/Cart.php
@@ -3,12 +3,11 @@
 <section class="cart">
   <div class="overlay"></div>
   <div class="container">
-    <?php if(!empty($_GET['order'])){?> <script>alert("Đơn hàng đã được gởi")</script> <?php } ?>
     <h4>
       <ion-icon name="cart-outline"></ion-icon>
-      <span>Your Cart (<?= empty($_SESSION["Cart"])?0:count($_SESSION["Cart"])?>) </span>
+      <span>Giỏ hàng của bạn (<span name="dish-in-cart"><?= empty($_SESSION["Cart"])?0:count($_SESSION["Cart"])?></span>) </span>
     </h4>
-    <?php if(!empty($_SESSION["Cart"])){ ?>
+    <?php if(!empty($_SESSION["Cart"])){  ?>
 
     <div class="list">
       <?php foreach($_SESSION["Cart"] as $x => $val) { ?>
@@ -25,11 +24,11 @@
         <span class="price"> <span id="<?= $val['IDDISH']."Price"?>"><?= $val['Quantity']*$val['PRICE'];?></span> VNĐ</span>
         <button class="btn deletebtn" name="<?= $val['IDDISH']?>" onclick="deletedish(name)">Xóa</button></div>
       </div>
-    <?php } ?>    
+    <?php } ?>
     </div>
-    <form action="index.php?controller=Cart&action=addCartDB" method="POST">
+    <form id="carts">
     <?php if(empty($_SESSION['iduser']) && empty($_SESSION["idemp"]) && empty($_SESSION["idmanager"])){
-      ?> 
+      ?>
       <div style="margin:10px;">
         <label class="form-label">Họ và tên</label>
         <input type="text" name="fullname" class="form-control" placeholder="Nhập họ và tên" id="information1">
@@ -38,10 +37,8 @@
         <label class="form-label">Số điện thoại</label>
         <input type="text" name="phone" class="form-control" placeholder="Nhập số điện thoại" id="information2">
       </div>
-      <div style="margin:10px;">
-        <label >Số điện thoại</label>
-        <input type="text" name="phone" placeholder="Nhập số điện thoại">
-      </div>  <?php } 
+      
+      <?php }
       if(empty($_SESSION["idemp"]) && empty($_SESSION["idmanager"])){ ?>
         <div id="selectListed">
           <br>
@@ -59,5 +56,4 @@
       } else { ?> <h4 class="item">Không có sản phẩm nào trong giỏ</h4>  <?php }  ?>
     </form>
   </div>
-
 </section>
